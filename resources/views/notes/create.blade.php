cca36fcecaea1e0459ca44d81574825766d18a11
Duilio Palacios <i@duilio.me>
2016-02-19 17:14:23 +0000

    Improve create form design

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,10 +1,16 @@
 @extends('layout')
 
 @section('content')
-    <h1>Create a note</h1>
-    <form method="POST" action="{{ url('notes') }}" class="form">
-        {!! csrf_field() !!}
-        <textarea name="note" class="form-control"></textarea>
-        <button type="submit" class="btn btn-primary">Create note</button>
-    </form>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <h2>Create a note</h2>
+            <form method="POST" action="{{ url('notes') }}" class="form">
+                {!! csrf_field() !!}
+                <div class="form-group">
+                    <textarea name="note" class="form-control" placeholder="Write your note here..."></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create note</button>
+            </form>
+        </div>
+    </div>
 @endsection
